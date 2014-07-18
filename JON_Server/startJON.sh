15ffc9ac371945e28f984c93ebbbee156bd7a888
Patrick Steiner <patrick@steiner-buchholz.de>
2014-07-18 18:42:48 +0200

    New directory

---
+++ JON_Server/startJON.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export PGDATA="/var/lib/pgsql/9.3/data"
+export PGINST="/usr/pgsql-9.3"
+export RHQ_SERVER_HOME="/opt/jon/jon-server-3.2.0.GA"
+
+#$PGINST/bin/pg_ctl start -w -D $PGDATA
+
+service postgresql-9.3 start
+
+$RHQ_SERVER_HOME/bin/rhqctl install --start
+
+# endless look, as I do not have any other idea how to prevent the docker
+# container to stop
+for (( ; ; ))
+do
+   sleep 10000
+done
+
