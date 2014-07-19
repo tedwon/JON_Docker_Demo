afb76d555a3ce2012d8f939e68a9d14a5b87d572
Patrick Steiner <psteiner@localhost.localdomain>
2014-07-19 21:29:56 +0200

    added Template

---
+++ managedTemplate/appStarterScript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###############################################################
+# Replace all the lines below this comment with the code
+# to start your app, but please be aware that it should
+# not run in deamon mode as a termination of this script
+# also terminates the container.
+###############################################################
+
+for (( ; ; ))
+do
+   sleep 10000
+done
